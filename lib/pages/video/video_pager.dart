5e7c45da7a6749ffe2ab90000e98efbefa4f4de7
wanchunli <1596900283@qq.com>
2020-04-10 18:32:34 +0800

    新增抖音界面

--- lib/pages/video/video_pager.dart
+++ lib/pages/video/video_pager.dart
@@ -22,7 +22,7 @@ class _VideoItemPager extends State<VideoItemPager>
   VideoPlayerController _controller;
   Future videoFuture;
 
-  AnimationController controller;
+  AnimationController animController;
 
   @override
   void initState() {
@@ -32,26 +32,31 @@ class _VideoItemPager extends State<VideoItemPager>
     _controller.setLooping(true);
     videoFuture = _controller.initialize().then((_) {
       _controller.play();
-      setState(() {});
+      setState(() {
+        animController.forward();
+      });
     });
     //AnimationController是一个特殊的Animation对象，在屏幕刷新的每一帧，就会生成一个新的值，
     // 默认情况下，AnimationController在给定的时间段内会线性的生成从0.0到1.0的数字
     //用来控制动画的开始与结束以及设置动画的监听
     //vsync参数，存在vsync时会防止屏幕外动画（动画的UI不在当前屏幕时）消耗不必要的资源
     //duration 动画的时长，这里设置的 seconds: 2 为2秒，当然也可以设置毫秒 milliseconds：2000.
-    controller =
-        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animController =
+        AnimationController(duration: const Duration(seconds: 5), vsync: this);
     //动画开始、结束、向前移动或向后移动时会调用StatusListener
-    controller.addStatusListener((status) {
+    animController.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
         //动画从 controller.forward() 正向执行 结束时会回调此方法
         print("status is completed");
+        //重置起点
+        animController.reset();
+        //开启
+        animController.forward();
       } else if (status == AnimationStatus.dismissed) {
         //动画从 controller.reverse() 反向执行 结束时会回调此方法
         print("status is dismissed");
       } else if (status == AnimationStatus.forward) {
         print("status is forward");
-        //执行 controller.forward() 会回调此状态
       } else if (status == AnimationStatus.reverse) {
         //执行 controller.reverse() 会回调此状态
         print("status is reverse");
@@ -207,7 +212,7 @@ class _VideoItemPager extends State<VideoItemPager>
 
   Widget _buildUserInfoWidget() {
     return Align(
-      alignment: Alignment(1, 0.6),
+      alignment: Alignment(1, 0.5),
       child: Container(
         width: 60,
         child: Column(
@@ -355,21 +360,21 @@ class _VideoItemPager extends State<VideoItemPager>
   Widget _buildMusicWidget() {
     //旋转
     return Align(
-      alignment: Alignment(0, 1),
+      alignment: Alignment(1, 1),
       child: Container(
-        alignment: Alignment.center,
+        margin: EdgeInsets.all(10),
         child: RotationTransition(
           //设置动画的旋转中心
           alignment: Alignment.center,
           //动画控制器
-          turns: controller,
+          turns: animController,
           //将要执行动画的子view
           child: Container(
-            width: 40,
-            height: 40,
+            width: 44,
+            height: 44,
             padding: EdgeInsets.all(8),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
+              borderRadius: BorderRadius.all(Radius.circular(22)),
               color: Colors.grey,
             ),
             child: CircleAvatar(
